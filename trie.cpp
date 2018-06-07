ce5e8d1c2de8dca42649e001cb552b64a90b66a3
Piyush Bharti <piyush.23.12.1996@gmail.com>
2018-06-07 10:09:13 +0530

    Search with Display

--- trie.cpp
+++ trie.cpp
@@ -7,19 +7,19 @@
 #define pb push_back
 using namespace std;
 struct Node{
-    map <char , Node * > m;
-    bool eow;
+  map <char , Node *> m;
+  int now;
 };
 void insert(string s , Node *root)
 {
-    Node *temp=root;
+    Node *temp = root;
     for(int i=0;i<s.length();i++)
     {
         if(temp->m[s[i]]==NULL)
         {
-            Node *nn = new Node;
+            Node *nn = new Node();
             temp->m[s[i]] = nn;
-            nn->eow = false;
+            nn->now = 0;
             temp = nn;
         }
         else
@@ -27,41 +27,63 @@ void insert(string s , Node *root)
             temp = temp->m[s[i]];
         }
     }
-    temp->eow = true;
-
+    temp->now++;
+}
+void display(string x , Node *root)
+{
+    if(root->now >= 1){
+       for(int i=1;i<=root->now;i++)
+       {
+           cout<<x<<endl;
+       }
+    }
+    map <char , Node * > ::iterator it;
+    it = root->m.begin();
+    while(it!=root->m.end())
+    {
+        x = x + it->first;
+        display(x ,it->second);
+        x = x.substr(0,x.length()-1);
+        it++;
+    }
 }
 bool search(string s , Node *root)
 {
-    Node* temp = root;
+    Node *temp = root;
     for(int i=0;i<s.length();i++)
     {
         if(temp->m[s[i]]==NULL)
+        {
             return false;
-        else
-            temp = temp->m[s[i]];
+        }
+        temp = temp->m[s[i]];
     }
+    display(s,temp);
     return true;
 }
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n ,i;
+    int n,i;
     string s;
     cin>>n;
     Node *root = new Node();
-    root->eow = false;
     for(i=1;i<=n;i++)
     {
         cin>>s;
         insert(s , root);
     }
+
     cin>>n;
     for(i=1;i<=n;i++)
     {
         cin>>s;
-        cout<<search(s,root)<<endl;
+        if(!search(s ,root))
+        {
+            cout<<"Not Found"<<endl;
+        }
     }
-}
 
+}
 
